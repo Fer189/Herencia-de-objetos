e347bc0d17f5a68e253f6e1fd74d7be411f5ca28
FERNANDO ISMAEL NAVARRETE RUIZ <a21310393@live.ceti.mx>
2022-03-24 15:48:39 -0600

    Added Picaxe

--- Picaxe.h
+++ Picaxe.h
@@ -8,51 +8,51 @@
 #define DIAMOND 5
 using namespace std;
 
-class Axe : protected Tools {
+class Picaxe : protected Tools {
     public:
-    Axe(string name, int material) {
-        sharpness = 100;
+    Picaxe(string name, int material) {
+        hardness = 100;
         setName(name);
         setHealth(100);
         switch(material) {
             case WOOD:
             setDurability(150);
-            setDamage(50);
+            setDamage(25);
             break;
             case STONE:
-            setDurability(200);
-            setDamage(75);
+            setDurability(220);
+            setDamage(40);
             break;
             case STEEL:
-            setDurability(250);
-            setDamage(100);
+            setDurability(280);
+            setDamage(50);
             break;
             case GOLD:
-            setDurability(300);
-            setDamage(175);
+            setDurability(340);
+            setDamage(90);
             break;
             case DIAMOND:
-            setDurability(400);
-            setDamage(225);
+            setDurability(450);
+            setDamage(110);
             break;
         }
     }
-    void showAxe() {
+    void showPicaxe() {
         cout << "Salud: " << getHealth() << endl;
         cout << "Nombre: " << getName() << endl;
         cout << "Durabilidad " << getDurability() << endl;
     }
     void attack() {
         setHealth(getHealth() - (1000 / getDurability()));
-        cout << "Daño realizado: " << (sharpness * getDamage() / 1000) << endl;
+        cout << "Daño realizado: " << (hardness * getDamage() / 1000) << endl;
     }
     void repair() {
         setHealth(100);
     }
-    void attackTree() {
+    void attackMineralOre() {
         setHealth(getHealth() - (1000 / getDurability() / 2));
-        cout << "Daño realizado: " << (sharpness * getDamage() * 2 / 1000) << endl;
+        cout << "Daño realizado: " << (hardness * getDamage() * 2 / 1000) << endl;
     }
     private:
-    int sharpness;
+    int hardness;
 };
--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "Sword.h"
 #include "tools.h"
 #include "Axe.h"
+#include "Picaxe.h"
 #define WOOD 1
 #define STONE 2
 #define STEEL 3
@@ -12,6 +13,7 @@ using namespace std;
 int main() {
     Sword sword("MataPuercos", DIAMOND);
     Axe axe("CortaTroncos", DIAMOND);
+    Picaxe picaxe("PikaPiedras", WOOD);
     sword.showSword();
     sword.attack();
     sword.showSword();
@@ -21,5 +23,10 @@ int main() {
     axe.attack();
     axe.attackTree();
     axe.showAxe();
+    cout << " " << endl;
+    picaxe.showPicaxe();
+    picaxe.attack();
+    picaxe.attackMineralOre();
+    picaxe.showPicaxe();
     return 0;
 }
